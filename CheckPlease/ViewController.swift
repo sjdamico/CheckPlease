efd7f523ed35baaedbb3654b118fd2fdbad33ed2
StevesRepository <sjdamico203@sbcglobal.net>
2016-04-16 23:15:22 -0500

    Initial Commit

---
+++ CheckPlease/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  CheckPlease
+//
+//  Created by Steve D'Amico on 4/16/16.
+//  Copyright © 2016 Steve D'Amico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let check = CheckMarkRecognizer(target: self, action: "doCheck:")
+        view.addGestureRecognizer(check)
+        imageView.hidden = true;
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func doCheck(check: CheckMarkRecognizer) {
+        imageView.hidden = false
+        if imageView.traitCollection.forceTouchCapability == .Available && check.maxPossibleForce > 0 {
+            imageView.alpha = CGFloat(check.greatestForce/check.maxPossibleForce)
+        } else {
+            imageView.alpha = CGFloat(1)
+        }
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(2 * NSEC_PER_SEC)),  dispatch_get_main_queue()) {
+            self.imageView.hidden = true
+        }
+    }
+}
+
